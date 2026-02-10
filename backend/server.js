eeb04408ce5e513b1f50b3de54a831de7355092c
Ragul <ragulragul8788@gmail.com>
2026-02-10 20:17:47 +0530

    initial backend setup checking

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+/*require - to import dotenv library
+  returns a js object dotenv
+  config is a method on that object which loads the .env values into process.env runtime object*/
+
+require("dotenv").config();
+
+/*importing mangoose object and express factory function  */
+const express = require("express");
+const mongoose = require("mongoose");
+
+/*calling the factory function will create the actual application object*/
+const app = express();
+
+/*use is adding the middleware to the application layer stack
+  express.json()*/
+
+app.use(express.json());
+
+/*connecting to mongo DB atlas using connect function on mongoose object 
+as it connection will take time as its asynchronous it returns promise
+we use then if success and catch if rejected*/
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo DB is connected"))
+  .catch((err) => console.log("Could not establish connection to mongo", err));
+
+/*when someone visits the / endpoint , run this call back function
+  send sets the header and closes the connection !*/
+app.get("/", (req, res) => res.send("backend is running"));
+
+const PORT = process.env.PORT || 5000;
+
+//connects to the port and listens there , enables a long running process
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
